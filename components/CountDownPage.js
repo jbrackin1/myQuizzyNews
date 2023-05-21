a7da0560febee68790002f7109afcf521fef0128
jbrackin1 <jbrackin1@gmail.com>
2023-05-21 00:53:09 -0500

    setTimeout effect now working

--- components/CountDownPage.js
+++ components/CountDownPage.js
@@ -1,21 +1,29 @@
-import { CountdownCircleTimer } from 'react-native-countdown-circle-timer'
-import {View, Text, StyleSheet} from 'react-native'
+import { useEffect } from 'react';
+import { CountdownCircleTimer } from 'react-native-countdown-circle-timer';
+import { View, Text, StyleSheet } from 'react-native';
 
-export default function CountDownPage()
- {
-   
-    return (
-      <View style={styles.timer}>
-          <CountdownCircleTimer
-          isPlaying
-          duration={7}
-          colors={['#004777', '#F7B801', '#A30000', '#A30000']}
-          colorsTime={[7, 5, 2, 0]}
-        >
-          {({ remainingTime }) => <Text>{remainingTime}</Text>}
-        </CountdownCircleTimer>
+export default function CountDownPage({ navigation }) {
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      navigation.navigate('Home');
+    }, 3000);
+
+    // Clean up the timeout when the component is unmounted or the effect is re-run
+    return () => clearTimeout(timeout);
+  }, []);
+
+  return (
+    <View style={styles.timer}>
+      <CountdownCircleTimer
+        isPlaying
+        duration={7}
+        colors={['#004777', '#F7B801', '#A30000', '#A30000']}
+        colorsTime={[7, 5, 2, 0]}
+      >
+        {({ remainingTime }) => <Text>{remainingTime}</Text>}
+      </CountdownCircleTimer>
     </View>
-    )
+  );
 }
 
 const styles = StyleSheet.create({
@@ -25,5 +33,4 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
   },
-})
- 
+});
