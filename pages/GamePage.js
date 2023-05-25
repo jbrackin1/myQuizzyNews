a5c1a4d2563382d777d6fd8f91d8c362754c905e
jbrackin1 <jbrackin1@gmail.com>
2023-05-25 05:31:12 -0500

    rough draft 2 gamepage

--- pages/GamePage.js
+++ pages/GamePage.js
@@ -1,56 +1,45 @@
 import { View, Text, StyleSheet, Pressable } from 'react-native';
 
-{/* <View style={styles.middleContainer}>
-<Text style={styles.middleText}>Ready?</Text>
-<Pressable style={styles.playButton} onPress={pressHandler}>
-<Text style={styles.playButtonText}>PLAY</Text>
-</Pressable>        
-</View> */}
-
 export default function GamePage(){
-    return (
-        <View style={styles.screenContainer}>
-            <View >            <Pressable style={styles.answerButton}>
-            <Text style={styles.answerText}>Something</Text></Pressable>
-            <Pressable style={styles.answerButton}>
-            <Text style={styles.answerText}>Something2</Text></Pressable>
-            <Pressable style={styles.answerButton}>
-            <Text style={styles.answerText}>Something</Text></Pressable>
-            </View>
-
-            {/* <Pressable style={styles.answerButton}>Something</Pressable>
-            <Pressable style={styles.answerButton}>Something</Pressable> */}
-           
-        </View>
-            
-    )
+  return (
+    <View style={styles.screenContainer}>
+      <View style={styles.buttonContainer}>
+        <Pressable style={styles.answerButton}>
+          <Text style={styles.answerText}>Something</Text>
+        </Pressable>
+        <Pressable style={styles.answerButton}>
+          <Text style={styles.answerText}>Something2</Text>
+        </Pressable>
+        <Pressable style={styles.answerButton}>
+          <Text style={styles.answerText}>Something</Text>
+        </Pressable>
+      </View>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    screenContainer: {
-      backgroundColor: '#DED1E4',
-      display: 'flex',
-      flex: 1,
-      justifyContent: 'center',
-      alignItems: 'center',
-    },
-    answerButton: {
-        display: 'flex',
-        backgroundColor: '#FFFFFF',
-        borderRadius: '10px',
-        width: '335px',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    answerText: {
-        fontSize: '22px', 
-        fontFamily: 'futura', 
-        display: 'flex', 
-        alignItems: 'center', 
-        justifyContent: 'center', 
-        width: '335px', 
-        height: '95px',
-        topMargin: '33px', 
-        radius:'10px',
-    }
-  });
+  screenContainer: {
+    backgroundColor: '#DED1E4',
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  buttonContainer: {
+    marginTop: 20, // Add margin to create space between buttons
+  },
+  answerButton: {
+    backgroundColor: '#FFFFFF',
+    borderRadius: 10,
+    width: 335,
+    height: 95,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginBottom: 10, // Add margin bottom to create space between buttons
+  },
+  answerText: {
+    fontSize: 22,
+    fontFamily: 'futura',
+    textAlign: 'center',
+  },
+});
