ec60da547db7fa559f05a6c7f697008371ef9c88
jbrackin1 <jbrackin1@gmail.com>
2023-05-27 01:32:49 -0500

    second draft game screen

--- pages/GamePage.js
+++ pages/GamePage.js
@@ -1,17 +1,56 @@
+import React, { useState } from 'react';
 import { View, Text, StyleSheet, Pressable } from 'react-native';
 
-export default function GamePage(){
+export default function GamePage() {
+  const [georgePressed, setGeorgePressed] = useState(false);
+  const [button1Pressed, setButton1Pressed] = useState(false);
+  const [button2Pressed, setButton2Pressed] = useState(false);
+
+  const handlePress = (button) => {
+    if (button === 'George') {
+      setGeorgePressed(true);
+      setButton1Pressed(false);
+      setButton2Pressed(false);
+    } else if (button === 'Button 1') {
+      setGeorgePressed(false);
+      setButton1Pressed(true);
+      setButton2Pressed(false);
+    } else if (button === 'Button 2') {
+      setGeorgePressed(false);
+      setButton1Pressed(false);
+      setButton2Pressed(true);
+    }
+  };
+
   return (
     <View style={styles.screenContainer}>
+      <View style={styles.questionContainer}>
+        <Text style={styles.questionText}>
+          Who was the first president of the US
+        </Text>
+      </View>
+
       <View style={styles.buttonContainer}>
-        <Pressable style={styles.answerButton}>
-          <Text style={styles.answerText}>Something</Text>
+        <Pressable
+          style={[
+            styles.answerButton,
+            georgePressed && styles.greenButton,
+          ]}
+          onPress={() => handlePress('George')}
+        >
+          <Text style={styles.answerText}>George</Text>
         </Pressable>
-        <Pressable style={styles.answerButton}>
-          <Text style={styles.answerText}>Something2</Text>
+        <Pressable
+          style={[styles.answerButton, button1Pressed && styles.redButton]}
+          onPress={() => handlePress('Button 1')}
+        >
+          <Text style={styles.answerText}>Mountain Dew Elizonda The Third</Text>
         </Pressable>
-        <Pressable style={styles.answerButton}>
-          <Text style={styles.answerText}>Something</Text>
+        <Pressable
+          style={[styles.answerButton, button2Pressed && styles.redButton]}
+          onPress={() => handlePress('Button 2')}
+        >
+          <Text style={styles.answerText}>Joe Maldanado</Text>
         </Pressable>
       </View>
     </View>
@@ -25,8 +64,12 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
   },
+  questionContainer: {
+    display: 'flex',
+    alignItems: 'flex-start',
+  },
   buttonContainer: {
-    marginTop: 20, // Add margin to create space between buttons
+    marginTop: 20,
   },
   answerButton: {
     backgroundColor: '#FFFFFF',
@@ -35,11 +78,22 @@ const styles = StyleSheet.create({
     height: 95,
     alignItems: 'center',
     justifyContent: 'center',
-    marginBottom: 10, // Add margin bottom to create space between buttons
+    marginBottom: 10,
   },
   answerText: {
     fontSize: 22,
     fontFamily: 'futura',
     textAlign: 'center',
+  },
+  questionText: {
+    fontSize: 55,
+    fontFamily: 'sans-serif',
+    marginBottom: 30,
+  },
+  greenButton: {
+    backgroundColor: 'green',
+  },
+  redButton: {
+    backgroundColor: 'red',
   },
 });
